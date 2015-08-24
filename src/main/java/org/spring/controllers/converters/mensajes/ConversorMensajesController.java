a6eacbcdbc70e587f573cad18c9d339848dfe146
Francisco Rincón <francisco.rg@gmail.com>
2015-08-24 11:30:13 +0200

    Conversor mensajes

--- src/main/java/org/spring/controllers/converters/mensajes/ConversorMensajesController.java
+++ src/main/java/org/spring/controllers/converters/mensajes/ConversorMensajesController.java
@@ -5,22 +5,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/conversor-mensajes/*")
+import com.sun.syndication.feed.atom.Feed;
+import com.sun.syndication.feed.rss.Channel;
+
+@RequestMapping("/conversormensajes/*")
 @Controller
 public class ConversorMensajesController {
-	
+
 	// REQUEST URI:
 	// http://localhost:8080/ejemploSpringMVC/conversor-mensajes/xml
-	@RequestMapping(value="/xml", method=RequestMethod.GET)
+	@RequestMapping(value = "/xml", method = RequestMethod.GET)
 	public @ResponseBody JavaBean xml() {
 		return new JavaBean("Manzanas", "Renault");
 	}
-	
+
 	// REQUEST URI:
 	// http://localhost:8080/ejemploSpringMVC/conversor-mensajes/json
-	@RequestMapping(value="/json", method=RequestMethod.GET)
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
 	public @ResponseBody JavaBean json() {
 		return new JavaBean("Peras", "Ferrary");
+	}
+
+	// REQUEST URI:
+	// http://localhost:8080/ejemploSpringMVC/conversor-mensajes/atom
+	@RequestMapping(value = "/atom", method = RequestMethod.GET)
+	public @ResponseBody Feed atom() {
+		Feed feed = new Feed();
+		feed.setFeedType("atom_1.0");
+		feed.setTitle("Titulo de prueba atom");
+
+		return feed;
+	}
+
+	// REQUEST URI:
+	// http://localhost:8080/ejemploSpringMVC/conversor-mensajes/rss
+	@RequestMapping(value = "/rss", method = RequestMethod.GET)
+	public @ResponseBody Channel rss() {
+		Channel channel = new Channel();
+		channel.setFeedType("rss_2.0");
+		channel.setTitle("Titulo de prueba rss");
+		channel.setDescription("Cuerpo de la descripción");
+		channel.setLink("http://www.dicotraining.com");
+		return channel;
 	}
 
 }
